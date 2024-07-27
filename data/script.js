83a181865ad496b3fefb4269ee7f9e4f9bc20b96
BuzzeeeD <h3online@gmail.com>
2024-07-27 20:30:00 -0300

    Update script.js

--- data/script.js
+++ data/script.js
@@ -1,11 +1,21 @@
+// Filtra itens na galeria com base na categoria
+function filterItems(category) {
+    let items = document.querySelectorAll('.galeria .image-container');
+    items.forEach(item => {
+        if (category === 'todos' || item.classList.contains(category)) {
+            item.style.display = 'block';
+        } else {
+            item.style.display = 'none';
+        }
+    });
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     // Função para alternar a visibilidade do menu de usuário
     function toggleUserMenu(event) {
         event.stopPropagation(); // Previne que o evento se propague para outros elementos
         var userMenu = document.getElementById('user-menu');
-        if (userMenu) {
-            userMenu.style.display = userMenu.style.display === 'block' ? 'none' : 'block';
-        }
+        userMenu.style.display = userMenu.style.display === 'block' ? 'none' : 'block';
     }
 
     // Função para fechar menus quando clicar fora deles
@@ -13,25 +23,21 @@ document.addEventListener('DOMContentLoaded', () => {
         var userMenu = document.getElementById('user-menu');
         var dropdowns = document.querySelectorAll('.dropdown-content');
 
-        // Verificar se userMenu existe antes de tentar acessar suas propriedades
-        if (userMenu && !userMenu.contains(event.target) && event.target.id !== 'profile-button') {
+        // Fecha o menu de usuário se o clique não for nele ou no botão de perfil
+        if (!userMenu.contains(event.target) && event.target.id !== 'profile-button') {
             userMenu.style.display = 'none';
         }
 
         // Fecha menus de produtos se o clique não for neles ou em seus acionadores
         dropdowns.forEach(menu => {
-            if (menu && !menu.contains(event.target) && !menu.parentElement.contains(event.target)) {
+            if (!menu.contains(event.target) && !menu.parentElement.contains(event.target)) {
                 menu.style.display = 'none';
             }
         });
     }
 
     // Ativa o menu de usuário quando o botão é clicado
-    var profileButton = document.getElementById('profile-button');
-    if (profileButton) {
-        profileButton.addEventListener('click', toggleUserMenu);
-    }
-
+    document.getElementById('profile-button').addEventListener('click', toggleUserMenu);
     // Fecha menus quando clicar em qualquer lugar fora deles
     document.addEventListener('click', closeMenus);
 
@@ -40,27 +46,18 @@ document.addEventListener('DOMContentLoaded', () => {
         toggle.addEventListener('click', function(event) {
             event.stopPropagation(); // Previne que o evento se propague para o documento
             var dropdownMenu = this.nextElementSibling;
-            if (dropdownMenu) {
-                dropdownMenu.style.display = dropdownMenu.style.display === 'block' ? 'none' : 'block';
-            }
+            dropdownMenu.style.display = dropdownMenu.style.display === 'block' ? 'none' : 'block';
         });
     });
 });
-
 // Função para abrir o popup
 function openPopup(popupId) {
-    var popup = document.getElementById(popupId);
-    if (popup) {
-        popup.style.display = "block";
-    }
+    document.getElementById(popupId).style.display = "block";
 }
 
 // Função para fechar o popup
 function closePopup(popupId) {
-    var popup = document.getElementById(popupId);
-    if (popup) {
-        popup.style.display = "none";
-    }
+    document.getElementById(popupId).style.display = "none";
 }
 
 // Fecha o popup se o usuário clicar fora do conteúdo
